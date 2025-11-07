6f4c4c049cc50657c2caae964b4ccf337e67985e
Muhamad Ulin Nuha <muhamadulinnuha653@gmail.com>
2025-11-07 20:23:21 +0700

    membuat tampilan awal

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:splash_screen/Screen/splash_screen1.dart';
+import 'package:splash_screen/Screen/splash_screen1.dart';
+
+void main() {
+  runApp(const MyApp()); // <- ini yang jalanin aplikasi
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "1123150002, Muhamad Ulin Nuha",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+    home: const Screen1(),
+    );
+  }
+}
