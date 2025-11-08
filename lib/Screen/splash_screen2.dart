b72b53034656f67b227ef03550f0b3c39aa6ddd3
Muhamad Ulin Nuha <muhamadulinnuha653@gmail.com>
2025-11-08 15:31:50 +0700

    membuat splash screen 2

---
+++ lib/Screen/splash_screen2.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+
+class Screen1 extends StatelessWidget {
+  const Screen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // agar konten di tengah
+          children: [
+            const SizedBox(height: 60), 
+
+            
+            Container(
+              width: 250,
+              height: 250,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blueAccent, // contoh warna latar
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            const Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            const Text(
+              "Forgot to bring your wallet \nwhen you are shopping?",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.green,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 5),
+
+             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 10,
+                  height: 10,
+                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFFedede9),  // aktif
+                  ),
+                ),
+                Container(
+                  width: 10,
+                  height: 10,
+                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFF84a98c),// nonaktif
+                  ),
+                ),
+                Container(
+                  width: 10,
+                  height: 10,
+                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFFedede9), // nonaktif
+                  ),
+                ),
+              ],
+            ),  
+
+            const Spacer(),
+
+             Container(
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: SizedBox(
+                width: double.infinity,
+                height: 45,
+                child: ElevatedButton(
+                  onPressed: () {
+                    
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF84a98c),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: const Text(
+                    "Continue",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 70),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
